71e7dcb018cba9bab1489544f8f40abcf8e29cf6
Valdez Bentiraz <bentirazv@gmail.com>
2024-11-21 14:20:46 +0700

    ini inisialisasi awal aplikasi

---
+++ src/test/java/dev/kel3pbo/todolist/TodolistApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.kel3pbo.todolist;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TodolistApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
