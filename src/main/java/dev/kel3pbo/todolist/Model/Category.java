40d1c3a9384ad643c252861cb8ff48a24d83b885
Arya Sabda <147800904+Aakarshaka@users.noreply.github.com>
2024-11-21 15:08:10 +0700

    editTipis2Category

---
+++ src/main/java/dev/kel3pbo/todolist/Model/Category.java
@@ -0,0 +1,13 @@
+package dev.kel3pbo.todolist.Model;
+import java.util.ArrayList;
+
+public class Category {
+    private String name;
+    private ArrayList<task> taks;
+
+    public Category(String name){
+        this.name = name;
+    }
+
+    
+}
