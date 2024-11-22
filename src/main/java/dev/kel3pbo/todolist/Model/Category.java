09a6aa576e230b4f2a53b74c5b42a99ab6c703f5
Arya Sabda <147800904+Aakarshaka@users.noreply.github.com>
2024-11-22 11:42:20 +0700

    update lagi

--- src/main/java/dev/kel3pbo/todolist/Model/Category.java
+++ src/main/java/dev/kel3pbo/todolist/Model/Category.java
@@ -1,31 +1,40 @@
 package dev.kel3pbo.todolist.Model;
 import java.util.ArrayList;
+import java.util.List;
 
 public class Category {
     private String name;
-    private ArrayList<Task> taks;
+    private List<Task> tasks;
 
-    public Category(String name){
-        setName();
+    // Constructor
+    public Category(String name) {
+        setName(name);
+        this.tasks = new ArrayList<>();
     }
 
-    public void addTask(Task task){
-
+    // Getter for name
+    public String getName() {
+        return name;
     }
 
-    public void removeTask(Task task){
-
-    }
-
-    public ArrayList<Task> getTask(){
-        return
-    }
-
-    public void setName(){
+    // Setter for name
+    public void setName(String name) {
         this.name = name;
     }
 
-    public String getName(){
-        return name;
+    // Add a task to the category
+    public void addTask(Task task) {
+        tasks.add(task);
+    }
+
+    // Remove a task from the category
+    public void removeTask(Task task) {
+        tasks.remove(task);
+    }
+
+    // Get the list of tasks in the category
+    public List<Task> getTasks() {
+        return tasks;
     }
 }
+
